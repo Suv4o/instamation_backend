1de944e2f1c14fe1626dad3d75d321f49f764424
Aleks <aleksandar.trpkovski@gmail.com>
2023-07-26 21:28:23 +1000

    feat(app.py): add Flask Blueprint and Flask-RESTful API to improve modularity and scalability feat(app.py): add HelloWorld resource to handle GET requests to the root endpoint feat(request.http): add test request to verify the functionality of the API endpoint

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restful import Resource, Api
 from waitress import serve
 from config import SERVER_HOST, SERVER_PORT, Logger
 
 
 app = Flask("app")
+api = Api(app, prefix="/api")
 console = Logger("app").get()
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"hello": "world"}
+
+
+api.add_resource(HelloWorld, "/")
 
 if __name__ == "__main__":
     console.info("Starting server...")
